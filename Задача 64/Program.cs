f6c85ca2adca2c1f7948ef525239959063812115
Demian <kilka228378@gmail.com>
2023-02-14 16:25:27 +0300

    Add repo

---
+++ Задача 64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int Recursion(int numberN)
+{
+    if(numberN == 0)return 1;
+    Console.Write(numberN);
+    if(numberN > 1)Console.Write(",");
+    else Console.Write("''");
+    Recursion(numberN - 1);
+    return numberN-1;
+}
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    return int.Parse(Console.ReadLine());
+}
+
+int numberN = GetNum("Введите значение числа N: ");
+Console.Write($"N = {numberN} -> ''");
+Recursion(numberN);
---
+++ Задача 66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Recursion(int M, int N, int result)
+{
+    if (M == N) return M;
+    result = M + Recursion(M + 1, N, result);
+    Console.WriteLine(result);
+    return result;
+}
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    return int.Parse(Console.ReadLine());
+}
+
+int numberM = GetNum("Введите значение числа M: ");
+int numberN = GetNum("Введите значение числа N: ");
+int result = 0;
+Recursion(numberM, numberN, result);
